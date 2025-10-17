3d7abd9e797d51470e704d198b11b0635f740fa6
Jeremy <cuthbert@pdx.edu>
2025-10-16 22:35:04 -0700

    Auto commit on 2025-10-16 22:35:04

--- cae-xor.c
+++ cae-xor.c
@@ -2,6 +2,8 @@
 // CS333 - Jesse Chaney
 // cae-xor.c
 
+// The purpose of this assignment is to implement the Caesar-like and XOR
+// cipher to encrypt/decrypt any message passed in by the user. 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +11,7 @@
 #include <ctype.h>
 #include <errno.h>
 
+// Macro for all options user can use on command line
 #define OPTIONS "edc:x:hD"
 
 int main(int argc, char *argv[])
@@ -17,29 +20,38 @@ int main(int argc, char *argv[])
   char *xor_string = NULL;
   bool encrypt = true; // change this value based on the command line (-e or -d)
 
+  // This reads/parses the command line input and will perform the respective
+  // action based on the option(s) provided by the user via stdin.
   {
     int opt = 0;
 
     while ((opt = getopt(argc, argv, OPTIONS) != -1) {
       switch (opt) {
+	// encrypt (default)
       case 'e':
 	
 	break;
+	// decrypt
       case 'd':
 
 	break;
+	// caeser encryption
       case 'c':
 
 	break;
+	// xor encryption
       case 'x':
 
 	break;
+	// Help about command line options
       case 'h':
 
 	break;
+	// Diagnostics about the process (optional)
       case 'D':
 
 	break;
+	// Handles incorrect/unknown input on command line
       default:
 	fprintf(stderr, "unknown option %c\n", optopt);
 	exit(EXIT_FAILURE);
